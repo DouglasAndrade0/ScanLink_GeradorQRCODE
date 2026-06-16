9842fb4126bdee0f1b99d548c6682f3c62b5071a
Douglas <154425029+DouglasAndrade0@users.noreply.github.com>
2026-06-16 15:40:25 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById("QRCODE_url");
-const button = document.getElementById("QRCODE_gerar");
-const image = document.getElementById("QRCODE_img");
-
-button.addEventListener("click", () => {
-    QRCode.toDataURL(input.value, function(err, url) {
-        image.src = url;
-    });
-});
