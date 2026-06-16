35b16b33feffb25039de32f56644c42508c9bce5
Douglas <154425029+DouglasAndrade0@users.noreply.github.com>
2026-06-16 15:41:54 -0300

    Update script.js

--- ScanLink/script.js
+++ ScanLink/script.js
@@ -1,1 +1,20 @@
+const input = document.getElementById("QRCODE_url");
+const button = document.getElementById("QRCODE_gerar");
+const image = document.getElementById("QRCODE_img");
 
+button.addEventListener("click", () => {
+    QRCode.toDataURL(input.value, {
+        color: {
+            dark: "#58E5B0", 
+            light: "#0D1117"  
+        }
+    }, function(err, url) {
+        image.src = url;
+        image.style.display = "block";
+    });
+});
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        button.click();
+    }
+});
